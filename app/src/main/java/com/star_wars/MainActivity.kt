f5d4cb5f39c0e72abc32c72df0d44655182faf40
Ray Carlos <raycarlos17@gmail.com>
2021-05-05 20:34:28 -0300

    Criação da tela de login e tela opçoes

--- app/src/main/java/com/star_wars/MainActivity.kt
+++ app/src/main/java/com/star_wars/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.star_wars
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_conectar.setOnClickListener{
+            conectarApp()
+        }
+
     }
+
+    private fun conectarApp() {
+        val intent = Intent(this, MenuPrincipal::class.java)
+        startActivity(intent)
+    }
+
 }
