01c2359f56b8b38870b7cd78109efc92ceb2f036
Ray Carlos <raycarlos17@gmail.com>
2021-05-07 00:01:18 -0300

    Criação da tela de lista de planetas, detalhes do planeta

---
+++ app/src/main/java/com/star_wars/DetalhesPlaneta.kt
@@ -0,0 +1,11 @@
+package com.star_wars
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetalhesPlaneta : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalhes_planeta)
+    }
+}
---
+++ app/src/main/java/com/star_wars/GaleriaPersonagens.kt
@@ -0,0 +1,11 @@
+package com.star_wars
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GaleriaPersonagens : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_galeria_personagens)
+    }
+}
